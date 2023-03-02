12bb1f279eb539f0821c63fa8511c924845f7686
Nathan Boaldin <nathan.boaldin@banno.com>
2023-03-02 16:59:57 -0600

    things

--- nvim/lua/user/nvim-tree.lua
+++ nvim/lua/user/nvim-tree.lua
@@ -10,6 +10,18 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+local HEIGHT_RATIO = 0.8 -- You can change this
+local WIDTH_RATIO = 0.5 -- You can change this too
+local screen_w = vim.opt.columns:get()
+local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+local window_w = screen_w * WIDTH_RATIO
+local window_h = screen_h * HEIGHT_RATIO
+local window_w_int = math.floor(window_w)
+local window_h_int = math.floor(window_h)
+local center_x = (screen_w - window_w) / 2
+local center_y = ((vim.opt.lines:get() - window_h) / 2)
+    - vim.opt.cmdheight:get()
+
 nvim_tree.setup {
   update_focused_file = {
     enable = true,
@@ -61,8 +73,21 @@ nvim_tree.setup {
     },
   },
   view = {
-    width = 30,
-    side = "left",
+    float = {
+      enable = true,
+      open_win_config = {
+        border = "rounded",
+        relative = "editor",
+        row = center_y,
+        col = center_x,
+        width = window_w_int,
+        height = window_h_int,
+      }
+      ,
+    },
+    width = function()
+      return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+    end,
     mappings = {
       list = {
         { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
@@ -74,9 +99,11 @@ nvim_tree.setup {
 }
 
 
-vim.api.nvim_create_autocmd({ "VimEnter" }, { 
-  callback = function()  
-      require("nvim-tree.api").tree.open()
+vim.api.nvim_create_autocmd({ "VimEnter" }, {
+  callback = function()
+    require("nvim-tree.api").tree.close()
+    require("nvim-tree.api").tree.toggle({
+      focus = true,
+    })
   end
 })
-
