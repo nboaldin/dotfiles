4607a763b7871b50b4a34b93a76cc634e6623fb7
Nathan Boaldin <nathan.boaldin@banno.com>
2023-04-05 19:12:32 -0500

    things

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -4,11 +4,10 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = {"rust", "go", "markdown", "javascript", "terraform", "json", "lua"}, -- one of "all" or a list of languages
+	ensure_installed = {"rust", "go", "markdown", "javascript", "terraform", "json", "lua", "toml", "yaml", "css", "bash", "vue"}, -- one of "all" or a list of languages
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
 		enable = true, -- false will disable the whole extension
-		disable = { "css" }, -- list of language that will be disabled
 	},
 	autopairs = {
 		enable = true,
