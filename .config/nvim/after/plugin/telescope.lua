a073512a875a1db2850dd9348dce7c87b1b91ad3
nboaldin <nboaldin@jackhenry.com>
2025-04-09 16:43:35 -0500

    updates

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -1,13 +1,16 @@
 require("telescope").setup({
-	pickers = {
-		find_files = {
-			hidden = true,
-		},
-		grep_string = {
-			additional_args = { "--hidden" },
-		},
-		live_grep = {
-			additional_args = { "--hidden" },
-		},
-	},
+    defaults = {
+        file_ignore_patterns = { "^%.git/" } -- Ignores .git/ but not .github/
+    },
+    pickers = {
+        find_files = {
+            hidden = true,
+        },
+        grep_string = {
+            additional_args = { "--hidden" },
+        },
+        live_grep = {
+            additional_args = { "--hidden" },
+        },
+    },
 })
